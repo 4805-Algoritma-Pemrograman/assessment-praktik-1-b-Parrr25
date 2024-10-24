4f3ed2e485ac3b2840626dc2da8030d9dc2ec1ea
Parrr25 <126962860+Parrr25@users.noreply.github.com>
2024-10-24 09:47:12 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,7 +1,31 @@
 
+import java.util.Scanner;
 
-public class Nomor1{
-    public static void main(String[] args){
-      
+public class Komisi {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        // Meminta input total penjualan
+        System.out.print("Total penjualan salesman: ");
+        double totalPenjualan = input.nextDouble();
+        
+        double komisi = 0.1;
+        double batasPenjualan = 500000;
+        
+        // Jika total penjualan kurang dari atau sama dengan 500.000
+        if (totalPenjualan <= batasPenjualan) {
+            komisi = totalPenjualan * 0.10;
+        } else {
+            // Komisi untuk 500.000 pertama
+            komisi = batasPenjualan * 0.1;
+            // Komisi untuk sisa penjualan di atas 500.000
+            komisi += (totalPenjualan - batasPenjualan) * 0.15;
+        }
+        
+        // Menampilkan hasil komisi
+        System.out.println("Total penjualan salesman: " + totalPenjualan);
+        System.out.println("Komisi yang didapatkan = " + komisi);
+        
+        input.close();
     }
 }
